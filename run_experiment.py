4f3dff0e2bb24f26ef8e2f121fac96b73a7d67c3
NicolasYanB <nicolas07.yan@gmail.com>
2025-12-17 17:03:24 -0300

    experiment execution script

---
+++ run_experiment.py
@@ -0,0 +1,29 @@
+import os
+
+models = [
+  ('KNN', 1),
+  ('SVM_lin', 1),
+  ('SVM_rbf', 1),
+  ('GLVQ', 5),
+  ('LR', 1),
+  ('GNB', 1),
+  ('GP', 17),
+  ('LDA', 1),
+  ('QDA', 1),
+  ('DT', 1),
+  ('MLP', 3),
+  ('Percep', 1),
+  ('XGBoost', 2),
+  ('RF', 3),
+  ('AdaBoost', 9),
+  ('Bagging', 17),
+  ('OLA', 17),
+  ('LCA', 17),
+  ('MCB', 17),
+  ('KNORAE', 17),
+  ('KNORAU', 17)
+]
+
+for model, division in models:
+  command = f'sbatch --account=def-menelau --mem=8000M --time=01:30:00 --job-name={model} --array=1-{division} array_job.sh {model}'
+  os.system(command)
