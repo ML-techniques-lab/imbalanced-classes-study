521cec20f09717663244fe6b198909a8aa587a7e
NicolasYanB <nicolas07.yan@gmail.com>
2025-12-17 08:54:19 -0300

    add argument to job

--- array_job.sh
+++ array_job.sh
@@ -6,4 +6,4 @@ mod=$(( 100 % $SLURM_ARRAY_TASK_MAX ))
 if [ $mod != 0 ] && [ $SLURM_ARRAY_TASK_ID == $SLURM_ARRAY_TASK_MAX]; then
   end=$(( $end + mod ))
 
-python experiment.py all GNB $start:$end "GNB_${step}.csv"
+python experiment.py all ${1} $start:$end "${1}_${step}.csv"
